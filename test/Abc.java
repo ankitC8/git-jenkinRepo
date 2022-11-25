cd9f1acc5ac9cc02183849cee46ab1fa86695c9e
Rahul Patil <rahul.patil@morningstar.com>
2022-11-25 12:46:59 +0530

    changes by rahul

--- test/Abc.java
+++ test/Abc.java
@@ -5,7 +5,12 @@ public class Abc {
 	public static void main(String[] args) {
 		for (int i = 0; i <=5; i++) 
 		{
-			System.out.println("hello duniya!!"+i);
+			System.out.println("hello duniya !!"+i);
+
+		}
+		for (int i = 0; i <=5; i++) 
+		{
+			System.out.println("Bye duniya !!"+i);
 
 		}
 	}
